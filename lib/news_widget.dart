1256138bad64342adb7c8b3f68d8f124c3f10b77
Kyle Machulis <kyle@nonpolynomial.com>
2022-09-10 12:25:25 -0700

    feat: Add markdown and basic news widget

--- lib/news_widget.dart
+++ lib/news_widget.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
 
 class NewsWidget extends StatelessWidget {
   const NewsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("This is some news");
+    return Expanded(
+        child: Markdown(
+      selectable: true,
+      data: 'Insert emoji :smiley: here',
+      extensionSet: md.ExtensionSet(
+        md.ExtensionSet.gitHubFlavored.blockSyntaxes,
+        [md.EmojiSyntax(), ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes],
+      ),
+    ));
   }
 }
