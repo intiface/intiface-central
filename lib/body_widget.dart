b16613c78c7abe703df16981c2b27a65c3f01a34
Kyle Machulis <kyle@nonpolynomial.com>
2022-09-10 16:52:24 -0700

    chore: Abstract navigation to work with bottom/side rails

--- lib/body_widget.dart
+++ lib/body_widget.dart
@@ -1,19 +1,20 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intiface_central/log_widget.dart';
 import 'package:intiface_central/navigation_cubit.dart';
 import 'package:intiface_central/news_widget.dart';
 import 'package:intiface_central/settings_widget.dart';
+import 'package:intiface_central/util/intiface_util.dart';
 
 class NavigationDestination {
   final bool Function(NavigationState state) stateCheck;
   final void Function(NavigationCubit cubit) navigate;
-  final NavigationRailDestination destination;
+  final IconData icon;
+  final IconData selectedIcon;
+  final String title;
   final Widget Function() widgetProvider;
 
-  NavigationDestination(this.stateCheck, this.navigate, this.destination, this.widgetProvider);
+  NavigationDestination(this.stateCheck, this.navigate, this.icon, this.selectedIcon, this.title, this.widgetProvider);
 }
 
 class BodyWidget extends StatelessWidget {
@@ -21,69 +22,33 @@ class BodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int _selectedIndex = 0;
     var destinations = [
-      NavigationDestination(
-          (state) => state is NavigationStateNews,
-          (NavigationCubit cubit) => cubit.goNews(),
-          const NavigationRailDestination(
-            icon: Icon(Icons.newspaper_outlined),
-            selectedIcon: Icon(Icons.newspaper),
-            label: Text('News'),
-          ),
-          () => const NewsWidget()),
-      NavigationDestination(
-          (state) => state is NavigationStateDevices,
-          (NavigationCubit cubit) => cubit.goDevices(),
-          const NavigationRailDestination(
-            icon: Icon(Icons.vibration_outlined),
-            selectedIcon: Icon(Icons.vibration),
-            label: Text('Devices'),
-          ),
-          () => const NewsWidget()),
-      NavigationDestination(
-          (state) => state is NavigationStateSettings,
-          (NavigationCubit cubit) => cubit.goSettings(),
-          const NavigationRailDestination(
-            icon: Icon(Icons.settings_outlined),
-            selectedIcon: Icon(Icons.settings),
-            label: Text('Settings'),
-          ),
-          () => const SettingWidget()),
-      NavigationDestination(
-          (state) => state is NavigationStateLogs,
-          (NavigationCubit cubit) => cubit.goLogs(),
-          const NavigationRailDestination(
-            icon: Icon(Icons.text_snippet_outlined),
-            selectedIcon: Icon(Icons.text_snippet),
-            label: Text('Log'),
-          ),
-          () => const LogWidget()),
-      NavigationDestination(
-          (state) => state is NavigationStateAbout,
-          (NavigationCubit cubit) => cubit.goAbout(),
-          const NavigationRailDestination(
-            icon: Icon(Icons.help_outlined),
-            selectedIcon: Icon(Icons.help),
-            label: Text('About'),
-          ),
-          () => const NewsWidget()),
+      NavigationDestination((state) => state is NavigationStateNews, (NavigationCubit cubit) => cubit.goNews(),
+          Icons.newspaper_outlined, Icons.newspaper, 'News', () => const NewsWidget()),
+      NavigationDestination((state) => state is NavigationStateDevices, (NavigationCubit cubit) => cubit.goDevices(),
+          Icons.vibration_outlined, Icons.vibration, 'Devices', () => const NewsWidget()),
+      NavigationDestination((state) => state is NavigationStateSettings, (NavigationCubit cubit) => cubit.goSettings(),
+          Icons.settings_outlined, Icons.settings, 'Settings', () => const SettingWidget()),
+      NavigationDestination((state) => state is NavigationStateLogs, (NavigationCubit cubit) => cubit.goLogs(),
+          Icons.text_snippet_outlined, Icons.text_snippet, 'Log', () => const LogWidget()),
+      NavigationDestination((state) => state is NavigationStateAbout, (NavigationCubit cubit) => cubit.goAbout(),
+          Icons.help_outlined, Icons.help, 'About', () => const NewsWidget()),
     ];
 
-    return BlocBuilder<NavigationCubit, NavigationState>(
-      builder: (context, state) {
-        var navCubit = BlocProvider.of<NavigationCubit>(context);
-        var selectedIndex = 0;
-        for (var element in destinations) {
-          if (element.stateCheck(state)) {
-            break;
-          }
-          selectedIndex += 1;
+    return BlocBuilder<NavigationCubit, NavigationState>(builder: (context, state) {
+      var navCubit = BlocProvider.of<NavigationCubit>(context);
+      var selectedIndex = 0;
+      for (var element in destinations) {
+        if (element.stateCheck(state)) {
+          break;
         }
-        if (selectedIndex >= destinations.length) {
-          selectedIndex = 0;
-        }
+        selectedIndex += 1;
+      }
+      if (selectedIndex >= destinations.length) {
+        selectedIndex = 0;
+      }
 
+      if (isDesktop()) {
         return Expanded(
             child: Row(children: <Widget>[
           NavigationRail(
@@ -93,13 +58,31 @@ class BodyWidget extends StatelessWidget {
                 destinations[index].navigate(navCubit);
               },
               labelType: NavigationRailLabelType.all,
-              destinations: destinations.map((v) => v.destination).toList()),
+              destinations: destinations
+                  .map((v) => NavigationRailDestination(icon: Icon(v.icon), label: Text(v.title)))
+                  .toList()),
           Expanded(
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [destinations[selectedIndex].widgetProvider()]))
         ]));
-      },
-    );
+      }
+      return Expanded(
+          child: Column(children: <Widget>[
+        Expanded(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, children: [destinations[selectedIndex].widgetProvider()])),
+        BottomNavigationBar(
+            currentIndex: selectedIndex,
+            onTap: (int index) {
+              destinations[index].navigate(navCubit);
+            },
+            type: BottomNavigationBarType.fixed,
+            items: destinations
+                .map((dest) => BottomNavigationBarItem(
+                    icon: Icon(dest.icon), activeIcon: Icon(dest.selectedIcon), label: dest.title))
+                .toList())
+      ]));
+    });
   }
 }
