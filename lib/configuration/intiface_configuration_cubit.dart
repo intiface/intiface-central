bb19a3ab686ef013998ce0a35aa516057c621e03
Kyle Machulis <kyle@nonpolynomial.com>
2022-09-10 12:25:02 -0700

    chore: Add DCM options to settings panel

--- lib/configuration/intiface_configuration_cubit.dart
+++ lib/configuration/intiface_configuration_cubit.dart
@@ -15,6 +15,142 @@ class ServerNameState extends IntifaceConfigurationState {
   final String value;
   ServerNameState(this.value);
 }
+
+class UseBluetoothLE extends IntifaceConfigurationState {
+  final bool value;
+  UseBluetoothLE(this.value);
+}
+
+class UseXInput extends IntifaceConfigurationState {
+  final bool value;
+  UseXInput(this.value);
+}
+
+class UseLovenseConnectService extends IntifaceConfigurationState {
+  final bool value;
+  UseLovenseConnectService(this.value);
+}
+
+class UseDeviceWebsocketServer extends IntifaceConfigurationState {
+  final bool value;
+  UseDeviceWebsocketServer(this.value);
+}
+
+class UseSerialPort extends IntifaceConfigurationState {
+  final bool value;
+  UseSerialPort(this.value);
+}
+
+class UseHID extends IntifaceConfigurationState {
+  final bool value;
+  UseHID(this.value);
+}
+
+class UseLovenseHIDDongle extends IntifaceConfigurationState {
+  final bool value;
+  UseLovenseHIDDongle(this.value);
+}
+
+class UseLovenseSerialDongle extends IntifaceConfigurationState {
+  final bool value;
+  UseLovenseSerialDongle(this.value);
+}
+
+class IntifaceConfigurationCubit extends Cubit<IntifaceConfigurationState> {
+  final IntifaceConfigurationRepository _repo;
+  IntifaceConfigurationCubit(this._repo) : super(IntifaceConfigurationStateNone());
+
+  set startServerOnStartup(bool value) {
+    _repo.startServerOnStartup = value;
+    emit(StartServerOnStartupState(value));
+  }
+
+  bool get startServerOnStartup {
+    return _repo.startServerOnStartup;
+  }
+
+  set serverName(String value) {
+    _repo.serverName = value;
+    emit(ServerNameState(value));
+  }
+
+  String get serverName {
+    return _repo.serverName;
+  }
+
+  set useBluetoothLE(bool value) {
+    _repo.withBluetoothLE = value;
+    emit(UseBluetoothLE(value));
+  }
+
+  bool get useBluetoothLE {
+    return _repo.withBluetoothLE;
+  }
+
+  set useXInput(bool value) {
+    _repo.withXInput = value;
+    emit(UseXInput(value));
+  }
+
+  bool get useXInput {
+    return _repo.withXInput;
+  }
+
+  set useLovenseConnectService(bool value) {
+    _repo.withLovenseConnectService = value;
+    emit(UseLovenseConnectService(value));
+  }
+
+  bool get useLovenseConnectService {
+    return _repo.withLovenseConnectService;
+  }
+
+  set useDeviceWebsocketServer(bool value) {
+    _repo.withDeviceWebsocketServer = value;
+    emit(UseDeviceWebsocketServer(value));
+  }
+
+  bool get useDeviceWebsocketServer {
+    return _repo.withDeviceWebsocketServer;
+  }
+
+  set useSerialPort(bool value) {
+    _repo.withSerialPort = value;
+    emit(UseSerialPort(value));
+  }
+
+  bool get useSerialPort {
+    return _repo.withSerialPort;
+  }
+
+  set useHID(bool value) {
+    _repo.withHID = value;
+    emit(UseHID(value));
+  }
+
+  bool get useHID {
+    return _repo.withHID;
+  }
+
+  set useLovenseHIDDongle(bool value) {
+    _repo.withLovenseHIDDongle = value;
+    emit(UseLovenseHIDDongle(value));
+  }
+
+  bool get useLovenseHIDDongle {
+    return _repo.withLovenseHIDDongle;
+  }
+
+  set useLovenseSerialDongle(bool value) {
+    _repo.withLovenseSerialDongle = value;
+    emit(UseLovenseSerialDongle(value));
+  }
+
+  bool get useLovenseSerialDongle {
+    return _repo.withLovenseSerialDongle;
+  }
+}
+
 /*
 enum IntifaceConfigurationStatus {
   none,
@@ -62,18 +198,3 @@ class IntifaceConfigurationState extends Equatable {
   List<Object?> get props => [status];
 }
 */
-
-class IntifaceConfigurationCubit extends Cubit<IntifaceConfigurationState> {
-  final IntifaceConfigurationRepository _repo;
-  IntifaceConfigurationCubit(this._repo) : super(IntifaceConfigurationStateNone());
-
-  set startServerOnStartup(bool value) {
-    _repo.startServerOnStartup = value;
-    emit(StartServerOnStartupState(value));
-  }
-
-  void serverName(String value) {
-    _repo.serverName = value;
-    emit(ServerNameState(value));
-  }
-}
--- lib/settings_widget.dart
+++ lib/settings_widget.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intiface_central/configuration/intiface_configuration_cubit.dart';
-import 'package:intiface_central/configuration/intiface_configuration_repository.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingWidget extends StatelessWidget {
@@ -10,33 +8,66 @@ class SettingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var repo = RepositoryProvider.of<IntifaceConfigurationRepository>(context);
     var cubit = BlocProvider.of<IntifaceConfigurationCubit>(context);
     return BlocBuilder<IntifaceConfigurationCubit, IntifaceConfigurationState>(
         builder: (context, state) => Expanded(
                 child: SettingsList(platform: DevicePlatform.windows, sections: [
-              //const SettingsSection(title: Text("Updates"), tiles: []),
+              SettingsSection(title: const Text("Updates"), tiles: [
+                CustomSettingsTile(child: TextButton(onPressed: () => {}, child: const Text("Check For Updates")))
+              ]),
               SettingsSection(title: const Text("Server Settings"), tiles: [
                 SettingsTile.switchTile(
-                    initialValue: repo.startServerOnStartup,
+                    initialValue: cubit.startServerOnStartup,
                     onToggle: (value) => cubit.startServerOnStartup = value,
                     title: const Text("Start Server when Intiface Central Launches")),
                 SettingsTile.navigation(
-                    title: const Text("Start Name"),
-                    value: Text(repo.serverName),
+                    title: const Text("Server Name"),
+                    value: Text(cubit.serverName),
                     onPressed: (context) {
                       showDialog(
                           context: context,
                           builder: (context) => AlertDialog(
-                                title: Text('TextField in Dialog'),
+                                title: const Text('TextField in Dialog'),
                                 content: TextField(
-                                  onChanged: (value) {
-                                    cubit.serverName(value);
+                                  controller: TextEditingController(text: cubit.serverName),
+                                  onSubmitted: (value) {
+                                    cubit.serverName = value;
+                                    Navigator.pop(context);
                                   },
-                                  decoration: InputDecoration(hintText: "Text Field in Dialog"),
+                                  decoration: const InputDecoration(hintText: "Text Field in Dialog"),
                                 ),
                               ));
                     })
+              ]),
+              SettingsSection(title: const Text("Device Managers"), tiles: [
+                SettingsTile.switchTile(
+                    initialValue: cubit.useBluetoothLE,
+                    onToggle: (value) => cubit.useBluetoothLE = value,
+                    title: const Text("Bluetooth LE")),
+                SettingsTile.switchTile(
+                    initialValue: cubit.useDeviceWebsocketServer,
+                    onToggle: (value) => cubit.useDeviceWebsocketServer = value,
+                    title: const Text("Device Websocket Server")),
+                SettingsTile.switchTile(
+                    initialValue: cubit.useXInput,
+                    onToggle: (value) => cubit.useXInput = value,
+                    title: const Text("XInput (Windows Only)")),
+                SettingsTile.switchTile(
+                    initialValue: cubit.useLovenseConnectService,
+                    onToggle: (value) => cubit.startServerOnStartup = value,
+                    title: const Text("Lovense Connect Service")),
+                SettingsTile.switchTile(
+                    initialValue: cubit.useLovenseHIDDongle,
+                    onToggle: (value) => cubit.useLovenseHIDDongle = value,
+                    title: const Text("Lovense HID Dongle")),
+                SettingsTile.switchTile(
+                    initialValue: cubit.useLovenseSerialDongle,
+                    onToggle: (value) => cubit.useLovenseSerialDongle = value,
+                    title: const Text("Lovense Serial Dongle")),
+                SettingsTile.switchTile(
+                    initialValue: cubit.useSerialPort,
+                    onToggle: (value) => cubit.useSerialPort = value,
+                    title: const Text("Serial Port")),
               ])
             ])));
   }
