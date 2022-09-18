a2f0c228f8d0f6c076afe297176cdbd87a3bcece
Kyle Machulis <kyle@nonpolynomial.com>
2022-09-17 18:29:47 -0700

    chore: Only show certain device settings on mobile

--- intiface_central/lib/settings_widget.dart
+++ intiface_central/lib/settings_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intiface_central/configuration/intiface_configuration_cubit.dart';
+import 'package:intiface_central/util/intiface_util.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingWidget extends StatelessWidget {
@@ -9,6 +10,42 @@ class SettingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var cubit = BlocProvider.of<IntifaceConfigurationCubit>(context);
+
+    var deviceSettings = [
+      SettingsTile.switchTile(
+          initialValue: cubit.useBluetoothLE,
+          onToggle: (value) => cubit.useBluetoothLE = value,
+          title: const Text("Bluetooth LE")),
+      SettingsTile.switchTile(
+          initialValue: cubit.useDeviceWebsocketServer,
+          onToggle: (value) => cubit.useDeviceWebsocketServer = value,
+          title: const Text("Device Websocket Server"))
+    ];
+    if (isDesktop()) {
+      deviceSettings.addAll([
+        SettingsTile.switchTile(
+            initialValue: cubit.useXInput,
+            onToggle: (value) => cubit.useXInput = value,
+            title: const Text("XInput (Windows Only)")),
+        SettingsTile.switchTile(
+            initialValue: cubit.useLovenseConnectService,
+            onToggle: (value) => cubit.startServerOnStartup = value,
+            title: const Text("Lovense Connect Service")),
+        SettingsTile.switchTile(
+            initialValue: cubit.useLovenseHIDDongle,
+            onToggle: (value) => cubit.useLovenseHIDDongle = value,
+            title: const Text("Lovense HID Dongle")),
+        SettingsTile.switchTile(
+            initialValue: cubit.useLovenseSerialDongle,
+            onToggle: (value) => cubit.useLovenseSerialDongle = value,
+            title: const Text("Lovense Serial Dongle")),
+        SettingsTile.switchTile(
+            initialValue: cubit.useSerialPort,
+            onToggle: (value) => cubit.useSerialPort = value,
+            title: const Text("Serial Port")),
+      ]);
+    }
+
     return BlocBuilder<IntifaceConfigurationCubit, IntifaceConfigurationState>(
         builder: (context, state) => Expanded(
                 child: SettingsList(platform: DevicePlatform.windows, sections: [
@@ -49,36 +86,7 @@ class SettingWidget extends StatelessWidget {
                               ));
                     })
               ]),
-              SettingsSection(title: const Text("Device Managers"), tiles: [
-                SettingsTile.switchTile(
-                    initialValue: cubit.useBluetoothLE,
-                    onToggle: (value) => cubit.useBluetoothLE = value,
-                    title: const Text("Bluetooth LE")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useDeviceWebsocketServer,
-                    onToggle: (value) => cubit.useDeviceWebsocketServer = value,
-                    title: const Text("Device Websocket Server")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useXInput,
-                    onToggle: (value) => cubit.useXInput = value,
-                    title: const Text("XInput (Windows Only)")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useLovenseConnectService,
-                    onToggle: (value) => cubit.startServerOnStartup = value,
-                    title: const Text("Lovense Connect Service")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useLovenseHIDDongle,
-                    onToggle: (value) => cubit.useLovenseHIDDongle = value,
-                    title: const Text("Lovense HID Dongle")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useLovenseSerialDongle,
-                    onToggle: (value) => cubit.useLovenseSerialDongle = value,
-                    title: const Text("Lovense Serial Dongle")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useSerialPort,
-                    onToggle: (value) => cubit.useSerialPort = value,
-                    title: const Text("Serial Port")),
-              ])
+              SettingsSection(title: const Text("Device Managers"), tiles: deviceSettings)
             ])));
   }
 }
