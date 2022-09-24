444ddb4144fc4215f23eab59866609de4a9b7f7d
Kyle Machulis <kyle@nonpolynomial.com>
2022-09-23 17:40:10 -0700

    chore: Only show About/Help in settings when using bottom rail

--- intiface_central/lib/settings_widget.dart
+++ intiface_central/lib/settings_widget.dart
@@ -10,100 +10,107 @@ class SettingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var cubit = BlocProvider.of<IntifaceConfigurationCubit>(context);
+    return BlocBuilder<IntifaceConfigurationCubit, IntifaceConfigurationState>(builder: (context, state) {
+      var cubit = BlocProvider.of<IntifaceConfigurationCubit>(context);
 
-    var deviceSettings = [
-      SettingsTile.switchTile(
-          initialValue: cubit.useBluetoothLE,
-          onToggle: (value) => cubit.useBluetoothLE = value,
-          title: const Text("Bluetooth LE")),
-      SettingsTile.switchTile(
-          initialValue: cubit.useDeviceWebsocketServer,
-          onToggle: (value) => cubit.useDeviceWebsocketServer = value,
-          title: const Text("Device Websocket Server"))
-    ];
-    if (isDesktop()) {
-      deviceSettings.addAll([
-        SettingsTile.switchTile(
-            initialValue: cubit.useXInput,
-            onToggle: (value) => cubit.useXInput = value,
-            title: const Text("XInput (Windows Only)")),
-        SettingsTile.switchTile(
-            initialValue: cubit.useLovenseConnectService,
-            onToggle: (value) => cubit.startServerOnStartup = value,
-            title: const Text("Lovense Connect Service")),
-        SettingsTile.switchTile(
-            initialValue: cubit.useLovenseHIDDongle,
-            onToggle: (value) => cubit.useLovenseHIDDongle = value,
-            title: const Text("Lovense HID Dongle")),
-        SettingsTile.switchTile(
-            initialValue: cubit.useLovenseSerialDongle,
-            onToggle: (value) => cubit.useLovenseSerialDongle = value,
-            title: const Text("Lovense Serial Dongle")),
-        SettingsTile.switchTile(
-            initialValue: cubit.useSerialPort,
-            onToggle: (value) => cubit.useSerialPort = value,
-            title: const Text("Serial Port")),
+      List<AbstractSettingsSection> tiles = [];
+
+      if (!cubit.useSideNavigationBar) {
+        tiles.add(SettingsSection(tiles: [
+          SettingsTile.navigation(
+              title: const Text("About"),
+              onPressed: (context) {
+                BlocProvider.of<NavigationCubit>(context).goAbout();
+              }),
+          SettingsTile.navigation(
+              title: const Text("Help"),
+              onPressed: (context) {
+                BlocProvider.of<NavigationCubit>(context).goHelp();
+              }),
+        ]));
+      }
+
+      tiles.addAll([
+        SettingsSection(title: const Text("Updates"), tiles: [
+          CustomSettingsTile(child: TextButton(onPressed: () => {}, child: const Text("Check For Updates")))
+        ]),
+        SettingsSection(title: const Text("App Settings"), tiles: [
+          SettingsTile.switchTile(
+              initialValue: cubit.useLightTheme,
+              onToggle: (value) => cubit.useLightTheme = value,
+              title: const Text("Light Theme")),
+          SettingsTile.switchTile(
+              initialValue: cubit.useSideNavigationBar,
+              onToggle: (value) => cubit.useSideNavigationBar = value,
+              title: const Text("Side Navigation Bar")),
+        ]),
+        SettingsSection(title: const Text("Server Settings"), tiles: [
+          SettingsTile.switchTile(
+              initialValue: cubit.startServerOnStartup,
+              onToggle: (value) => cubit.startServerOnStartup = value,
+              title: const Text("Start Server when Intiface Central Launches")),
+          SettingsTile.navigation(
+              title: const Text("Server Name"),
+              value: Text(cubit.serverName),
+              onPressed: (context) {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: const Text('TextField in Dialog'),
+                          content: TextField(
+                            controller: TextEditingController(text: cubit.serverName),
+                            onSubmitted: (value) {
+                              cubit.serverName = value;
+                              Navigator.pop(context);
+                            },
+                            decoration: const InputDecoration(hintText: "Text Field in Dialog"),
+                          ),
+                        ));
+              }),
+          SettingsTile.switchTile(
+              initialValue: cubit.websocketServerAllInterfaces,
+              onToggle: (value) => cubit.websocketServerAllInterfaces = value,
+              title: const Text("Listen on all network interfaces.")),
+        ])
       ]);
-    }
 
-    return BlocBuilder<IntifaceConfigurationCubit, IntifaceConfigurationState>(
-        builder: (context, state) => Expanded(
-                child: SettingsList(platform: DevicePlatform.windows, sections: [
-              SettingsSection(tiles: [
-                SettingsTile.navigation(
-                    title: const Text("About"),
-                    onPressed: (context) {
-                      BlocProvider.of<NavigationCubit>(context).goAbout();
-                    }),
-                SettingsTile.navigation(
-                    title: const Text("Help"),
-                    onPressed: (context) {
-                      BlocProvider.of<NavigationCubit>(context).goHelp();
-                    }),
-              ]),
-              SettingsSection(title: const Text("Updates"), tiles: [
-                CustomSettingsTile(child: TextButton(onPressed: () => {}, child: const Text("Check For Updates")))
-              ]),
-              SettingsSection(title: const Text("App Settings"), tiles: [
-                SettingsTile.switchTile(
-                    initialValue: cubit.useLightTheme,
-                    onToggle: (value) => cubit.useLightTheme = value,
-                    title: const Text("Light Theme")),
-                SettingsTile.switchTile(
-                    initialValue: cubit.useSideNavigationBar,
-                    onToggle: (value) => cubit.useSideNavigationBar = value,
-                    title: const Text("Side Navigation Bar")),
-              ]),
-              SettingsSection(title: const Text("Server Settings"), tiles: [
-                SettingsTile.switchTile(
-                    initialValue: cubit.startServerOnStartup,
-                    onToggle: (value) => cubit.startServerOnStartup = value,
-                    title: const Text("Start Server when Intiface Central Launches")),
-                SettingsTile.navigation(
-                    title: const Text("Server Name"),
-                    value: Text(cubit.serverName),
-                    onPressed: (context) {
-                      showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                title: const Text('TextField in Dialog'),
-                                content: TextField(
-                                  controller: TextEditingController(text: cubit.serverName),
-                                  onSubmitted: (value) {
-                                    cubit.serverName = value;
-                                    Navigator.pop(context);
-                                  },
-                                  decoration: const InputDecoration(hintText: "Text Field in Dialog"),
-                                ),
-                              ));
-                    }),
-                SettingsTile.switchTile(
-                    initialValue: cubit.websocketServerAllInterfaces,
-                    onToggle: (value) => cubit.websocketServerAllInterfaces = value,
-                    title: const Text("Listen on all network interfaces.")),
-              ]),
-              SettingsSection(title: const Text("Device Managers"), tiles: deviceSettings)
-            ])));
+      var deviceSettings = [
+        SettingsTile.switchTile(
+            initialValue: cubit.useBluetoothLE,
+            onToggle: (value) => cubit.useBluetoothLE = value,
+            title: const Text("Bluetooth LE")),
+        SettingsTile.switchTile(
+            initialValue: cubit.useDeviceWebsocketServer,
+            onToggle: (value) => cubit.useDeviceWebsocketServer = value,
+            title: const Text("Device Websocket Server"))
+      ];
+      if (isDesktop()) {
+        deviceSettings.addAll([
+          SettingsTile.switchTile(
+              initialValue: cubit.useXInput,
+              onToggle: (value) => cubit.useXInput = value,
+              title: const Text("XInput (Windows Only)")),
+          SettingsTile.switchTile(
+              initialValue: cubit.useLovenseConnectService,
+              onToggle: (value) => cubit.startServerOnStartup = value,
+              title: const Text("Lovense Connect Service")),
+          SettingsTile.switchTile(
+              initialValue: cubit.useLovenseHIDDongle,
+              onToggle: (value) => cubit.useLovenseHIDDongle = value,
+              title: const Text("Lovense HID Dongle")),
+          SettingsTile.switchTile(
+              initialValue: cubit.useLovenseSerialDongle,
+              onToggle: (value) => cubit.useLovenseSerialDongle = value,
+              title: const Text("Lovense Serial Dongle")),
+          SettingsTile.switchTile(
+              initialValue: cubit.useSerialPort,
+              onToggle: (value) => cubit.useSerialPort = value,
+              title: const Text("Serial Port")),
+        ]);
+      }
+
+      tiles.add(SettingsSection(title: const Text("Device Managers"), tiles: deviceSettings));
+      return Expanded(child: SettingsList(sections: tiles));
+    });
   }
 }
