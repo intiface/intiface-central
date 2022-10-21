b5e3e2ffa5ce72e347e218a629483a03757c3b5a
Kyle Machulis <kyle@nonpolynomial.com>
2022-10-20 19:24:50 -0700

    chore: Readjust buttplug/flutter ffi to work with all platforms

--- intiface_engine_flutter_plugin/intiface-engine-flutter-bridge/src/mobile_init/setup.rs
+++ intiface_engine_flutter_plugin/intiface-engine-flutter-bridge/src/mobile_init/setup.rs
@@ -3,15 +3,7 @@ mod android;
 #[cfg(target_os = "android")]
 pub use android::*;
 
-#[cfg(target_os = "ios")]
-mod ios;
-#[cfg(target_os = "ios")]
-pub use ios::*;
-
-// Dummy functions to silence rust-analyzer
-#[cfg(not(any(target_os = "android", target_os = "ios")))]
-pub static RUNTIME: OnceCell<Runtime> = once_cell::sync::OnceCell::new();
-#[cfg(not(any(target_os = "android", target_os = "ios")))]
-pub fn create_runtime(sink: flutter_rust_bridge::StreamSink<String>) -> Result<(), super::Error> {
-  Ok(())
-}
+#[cfg(not(target_os = "android"))]
+mod not_android;
+#[cfg(not(target_os = "android"))]
+pub use not_android::*;
--- intiface_engine_flutter_plugin/lib/ffi.dart
+++ intiface_engine_flutter_plugin/lib/ffi.dart
@@ -11,7 +11,11 @@ const _base = 'intiface_engine_flutter_bridge';
 // On MacOS, the dynamic library is not bundled with the binary,
 // but rather directly **linked** against the binary.
 
-final _dylib = io.Platform.isLinux ? 'native/target/debug/lib$_base.so' : 'lib$_base.so';
+final _dylib = io.Platform.isWindows
+    ? '$_base.dll'
+    : io.Platform.isLinux
+        ? 'native/target/debug/lib$_base.so'
+        : 'lib$_base.so';
 
 // The late modifier delays initializing the value until it is actually needed,
 // leaving precious little time for the program to quickly start up.
