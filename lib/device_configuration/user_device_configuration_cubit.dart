7fcfeec7cea66571e7d2df7d78749b9036bcd3a6
Kyle Machulis <kyle@nonpolynomial.com>
2023-04-22 13:20:45 -0700

    chore: Make sure identifier comparison works correctly

--- lib/device_configuration/user_device_configuration_cubit.dart
+++ lib/device_configuration/user_device_configuration_cubit.dart
@@ -1,12 +1,6 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:bloc/bloc.dart';
-import 'package:intiface_central/bridge_generated.dart';
-//import 'package:intiface_central/device_configuration/user_device_configuration_file.dart';
 import 'package:intiface_central/util/intiface_util.dart';
 import 'package:intiface_central/ffi.dart';
-import 'package:loggy/loggy.dart';
 
 class UserDeviceConfigurationState {}
 
@@ -14,12 +8,39 @@ class UserDeviceConfigurationStateInitial extends UserDeviceConfigurationState {
 
 class UserDeviceConfigurationStateUpdated extends UserDeviceConfigurationState {}
 
+class ExposedWritableUserDeviceConfig {
+  UserConfigDeviceIdentifier identifier;
+  String name;
+  String? displayName;
+  bool? allow;
+  bool? deny;
+  int? reservedIndex;
+
+  ExposedWritableUserDeviceConfig(
+      this.identifier, this.name, this.reservedIndex, this.displayName, this.allow, this.deny);
+
+  static ExposedWritableUserDeviceConfig fromRust(ExposedUserDeviceConfig config) {
+    return ExposedWritableUserDeviceConfig(
+        config.identifier, config.name, config.reservedIndex, config.displayName, config.allow, config.deny);
+  }
+
+  ExposedUserDeviceConfig toRust() {
+    return ExposedUserDeviceConfig(
+        identifier: identifier,
+        name: name,
+        reservedIndex: reservedIndex,
+        displayName: displayName,
+        allow: allow,
+        deny: deny);
+  }
+}
+
 class UserDeviceConfigurationCubit extends Cubit<UserDeviceConfigurationState> {
-  List<ExposedUserDeviceConfig> _configs = List.empty();
+  List<ExposedWritableUserDeviceConfig> _configs = List.empty();
 
   UserDeviceConfigurationCubit._() : super(UserDeviceConfigurationStateInitial());
 
-  List<ExposedUserDeviceConfig> get configs => _configs;
+  List<ExposedWritableUserDeviceConfig> get configs => _configs;
 
   static Future<UserDeviceConfigurationCubit> create() async {
     var cubit = UserDeviceConfigurationCubit._();
@@ -29,50 +50,78 @@ class UserDeviceConfigurationCubit extends Cubit<UserDeviceConfigurationState> {
     if (IntifacePaths.deviceConfigFile.existsSync() && IntifacePaths.userDeviceConfigFile.existsSync()) {
       var jsonDeviceConfig = IntifacePaths.deviceConfigFile.readAsStringSync();
       var jsonConfig = IntifacePaths.userDeviceConfigFile.readAsStringSync();
-      cubit._configs = await api.getUserDeviceConfigs(deviceConfigJson: jsonDeviceConfig, userConfigJson: jsonConfig);
+      cubit._configs = (await api.getUserDeviceConfigs(deviceConfigJson: jsonDeviceConfig, userConfigJson: jsonConfig))
+          .map((e) => ExposedWritableUserDeviceConfig.fromRust(e))
+          .toList();
     }
     return cubit;
   }
 
-  Future<void> updateDeviceIndex(UserConfigDeviceIdentifier deviceIdentifier, int index) async {
+  Future<void> updateDeviceAllow(UserConfigDeviceIdentifier deviceIdentifier, bool allow) async {
     // See if device already exists in config
-    ExposedUserDeviceConfig? newConfig;
     for (var config in _configs) {
-      if (config.identifier == deviceIdentifier) {
-        newConfig = ExposedUserDeviceConfig(
-            identifier: deviceIdentifier,
-            name: config.name,
-            displayName: config.displayName,
-            reservedIndex: index,
-            allow: config.allow,
-            deny: config.deny);
+      if (_compareIdentifiers(config.identifier, deviceIdentifier)) {
+        config.allow = allow;
+        await _saveConfigFile();
+        return;
       }
     }
-    if (newConfig != null) {
-      await _updateConfig(deviceIdentifier, newConfig);
+    ExposedWritableUserDeviceConfig newConfig =
+        ExposedWritableUserDeviceConfig(deviceIdentifier, "", null, null, allow, null);
+    await _updateConfig(deviceIdentifier, newConfig);
+  }
+
+  Future<void> updateDeviceDeny(UserConfigDeviceIdentifier deviceIdentifier, bool deny) async {
+    // See if device already exists in config
+    for (var config in _configs) {
+      if (_compareIdentifiers(config.identifier, deviceIdentifier)) {
+        config.deny = deny;
+        await _saveConfigFile();
+        return;
+      }
     }
+    ExposedWritableUserDeviceConfig newConfig =
+        ExposedWritableUserDeviceConfig(deviceIdentifier, "", null, null, null, deny);
+    await _updateConfig(deviceIdentifier, newConfig);
+  }
+
+  Future<void> updateDeviceIndex(UserConfigDeviceIdentifier deviceIdentifier, int reservedIndex) async {
+    // See if device already exists in config
+    for (var config in _configs) {
+      if (_compareIdentifiers(config.identifier, deviceIdentifier)) {
+        config.reservedIndex = reservedIndex;
+        await _saveConfigFile();
+        return;
+      }
+    }
+    ExposedWritableUserDeviceConfig newConfig =
+        ExposedWritableUserDeviceConfig(deviceIdentifier, "", reservedIndex, null, null, null);
+    await _updateConfig(deviceIdentifier, newConfig);
   }
 
   Future<void> updateDisplayName(UserConfigDeviceIdentifier deviceIdentifier, String displayName) async {
     // See if device already exists in config
-    ExposedUserDeviceConfig? newConfig;
     for (var config in _configs) {
-      if (config.identifier == deviceIdentifier) {
-        newConfig = ExposedUserDeviceConfig(
-            identifier: deviceIdentifier,
-            name: config.name,
-            displayName: displayName,
-            reservedIndex: config.reservedIndex,
-            allow: config.allow,
-            deny: config.deny);
+      if (_compareIdentifiers(config.identifier, deviceIdentifier)) {
+        config.displayName = displayName;
+        await _saveConfigFile();
+        return;
       }
     }
-    if (newConfig != null) {
-      await _updateConfig(deviceIdentifier, newConfig);
-    }
+    ExposedWritableUserDeviceConfig newConfig =
+        ExposedWritableUserDeviceConfig(deviceIdentifier, displayName, null, null, null, null);
+    await _updateConfig(deviceIdentifier, newConfig);
   }
 
-  Future<void> _updateConfig(UserConfigDeviceIdentifier deviceIdentifier, ExposedUserDeviceConfig newConfig) async {
+  // This is me being lazy about not wanting to implement equatable's immutable requirements.
+  bool _compareIdentifiers(UserConfigDeviceIdentifier ident1, UserConfigDeviceIdentifier ident2) {
+    return ident1.address == ident2.address &&
+        ident1.protocol == ident2.protocol &&
+        ident1.identifier == ident2.identifier;
+  }
+
+  Future<void> _updateConfig(
+      UserConfigDeviceIdentifier deviceIdentifier, ExposedWritableUserDeviceConfig newConfig) async {
     _configs.removeWhere((element) =>
         element.identifier.address == deviceIdentifier.address &&
         element.identifier.protocol == deviceIdentifier.protocol &&
@@ -82,7 +131,7 @@ class UserDeviceConfigurationCubit extends Cubit<UserDeviceConfigurationState> {
   }
 
   Future<void> _saveConfigFile() async {
-    var jsonString = await api.generateUserDeviceConfigFile(userConfig: _configs);
+    var jsonString = await api.generateUserDeviceConfigFile(userConfig: _configs.map((e) => e.toRust()).toList());
     await IntifacePaths.userDeviceConfigFile.writeAsString(jsonString);
     emit(UserDeviceConfigurationStateUpdated());
   }
