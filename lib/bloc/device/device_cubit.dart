d4d45b6921b014015cf4d6f12878f322f967db73
Kyle Machulis <kyle@nonpolynomial.com>
2025-12-28 22:54:55 -0800

    chore: Fix input property naming

--- lib/bloc/device/device_cubit.dart
+++ lib/bloc/device/device_cubit.dart
@@ -35,7 +35,7 @@ class DeviceCubit extends Cubit<DeviceState> {
       }
       if (feature.feature.input != null) {
         for (var input in feature.feature.input!.entries) {
-          _inputs.add(InputReadBloc(device, feature.feature.featureDescription, input.value.valueRange, input.key));
+          _inputs.add(InputReadBloc(device, feature.feature.featureDescription, input.value.value, input.key));
         }
       }
     }
